81146203c1d2321f4723fbb73d97b2d48df6c9e2
Dylan K. Taylor <odigiman@gmail.com>
2017-11-27 20:09:07 +0000

    lol

--- src/pocketmine/level/light/SkyLightUpdate.php
+++ src/pocketmine/level/light/SkyLightUpdate.php
@@ -26,10 +26,10 @@ namespace pocketmine\level\light;
 class SkyLightUpdate extends LightUpdate{
 
 	public function getLight(int $x, int $y, int $z) : int{
-		return $this->subChunkHandler->currentSubChunk->getBlockSkyLight($x, $y, $z);
+		return $this->subChunkHandler->currentSubChunk->getBlockSkyLight($x & 0x0f, $y & 0x0f, $z & 0x0f);
 	}
 
 	public function setLight(int $x, int $y, int $z, int $level){
-		$this->subChunkHandler->currentSubChunk->setBlockSkyLight($x, $y, $z, $level);
+		$this->subChunkHandler->currentSubChunk->setBlockSkyLight($x & 0x0f, $y & 0x0f, $z & 0x0f, $level);
 	}
 }
